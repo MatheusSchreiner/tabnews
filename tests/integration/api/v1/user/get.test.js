cf4a613eb82d6b09681b6e2e0d263bf83119e508
Matheus Schreiner <matheus.schreiner.ext@spms.min-saude.pt>
2025-12-01 14:53:51 +0000

    feat(dia 46 - 4): update `user.get.test` to use fakeTimer

--- tests/integration/api/v1/user/get.test.js
+++ tests/integration/api/v1/user/get.test.js
@@ -1,5 +1,6 @@
 import { version as uuidVersion } from "uuid";
 import orchestrator from "tests/orchestrator.js";
+import session from "models/session.js";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -38,6 +39,61 @@ describe("GET /api/v1/user", () => {
       expect(uuidVersion(createdUser.id)).toBe(4);
       expect(Date.parse(createdUser.created_at)).not.toBeNaN();
       expect(Date.parse(createdUser.updated_at)).not.toBeNaN();
+    });
+
+    test("With nonexistent session", async () => {
+      const nonexistentToken =
+        "d8719adff84f1735d3b085a7673bae5cb9c1d43139cf79b53ac51e4ff48c95b007831b6a478c0a0690b01af70ab41d39";
+
+      const response = await fetch("http://localhost:3000/api/v1/user", {
+        headers: {
+          Cookie: `session_id=${nonexistentToken}`,
+        },
+      });
+
+      expect(response.status).toBe(401);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "UnauthorizedError",
+        message: "Usuário não possui sessão ativa.",
+        action: "Verifique se este usuário está logado e tente novamente.",
+        status_code: 401,
+      });
+    });
+
+    test("With expired session", async () => {
+      // O Jest para o tempo da thread apenas deste teste, e com isto posso manipular o tempo como eu quiser
+      jest.useFakeTimers({
+        now: new Date(Date.now() - session.EXPIRATION_IN_MILLISECONDS),
+      });
+
+      const createdUser = await orchestrator.createUser({
+        username: "UserWithExpiredSession",
+      });
+
+      const sessionObject = await orchestrator.createSession(createdUser.id);
+
+      // Este método retorno a data/hora real
+      jest.useRealTimers();
+
+      const response = await fetch("http://localhost:3000/api/v1/user", {
+        headers: {
+          Cookie: `session_id=${sessionObject.token}`,
+        },
+      });
+
+      expect(response.status).toBe(401);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        name: "UnauthorizedError",
+        message: "Usuário não possui sessão ativa.",
+        action: "Verifique se este usuário está logado e tente novamente.",
+        status_code: 401,
+      });
     });
   });
 });
